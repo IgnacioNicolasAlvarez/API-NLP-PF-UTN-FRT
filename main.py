0cee2d64ebbe2cb35786d1e0ddf193f3f144ae1c
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2020-12-17 14:41:36 -0300

    se agregó spacy y procfile

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from fastapi import FastAPI
 import pickle
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
-import numpy as np 
-import pandas as  pd
+import numpy as np
+import pandas as pd
 from pydantic import BaseModel
+import spacy
+
 from enum import Enum
+
 
 class Clasificacion_Polaridad(Enum):
     NEGATIVO = 0
     POSITIVO = 1
     NEUTRO = 2
-    
+
+
 class Mensaje(BaseModel):
     texto: str
-
 
 
 app = FastAPI()
@@ -22,11 +25,23 @@ app = FastAPI()
 
 @app.post("/predecir")
 def read_root(mensaje: Mensaje):
-    archivo_modelo = 'modelo_transformador_proyecto.sav'
-    transformer, modelo = pickle.load(open(archivo_modelo, 'rb'))
-    print(mensaje)
-    x = pd.DataFrame([mensaje.texto])
+
+    nlp = spacy.load("es_core_news_sm")
+    tokens_limpios = []
+
+    doc = nlp(mensaje.texto.lower())
+    for token in doc:
+        if token.pos_ in ["NOUN", "VERB", "ADJ", "ADV"] and token.is_alpha:
+            tokens_limpios.append(token.lemma_)
+    texto = " ".join(tokens_limpios)
+
+    archivo_modelo = "modelo_transformador_proyecto.sav"
+    transformer, modelo = pickle.load(open(archivo_modelo, "rb"))
+
+    x = pd.DataFrame([texto])
     x = transformer.transform(x.iloc[0])
 
-    return {"Polaridad": f"{Clasificacion_Polaridad(modelo.predict(x)[0]).name}"}
-
+    return {
+        "Polaridad": f"{Clasificacion_Polaridad(modelo.predict(x)[0]).name}",
+        "Mensaje L": texto,
+    }
